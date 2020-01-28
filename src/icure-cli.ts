3c7a2babe363b945fc477dbfff1a0e3ba77344a6
Antoine Duchâteau <ad@taktik.com>
2020-01-28 11:02:41 +0100

    Fixed share

--- src/icure-cli.ts
+++ src/icure-cli.ts
@@ -157,47 +157,29 @@ vorpal
 	})
 
 vorpal
-	.command('export [patIds...]', 'Share with hcp ids')
-	.action(async function(this: CommandInstance, args: Args) {
-		let user = await api.usericc.getCurrentUser()
-
-		const hcpId = args.hcpId
-		const ids = args.patIds
-
-		const patients = await api.patienticc.getPatientsWithUser(user, new ListOfIdsDto({ ids })) // Get them to fix them
-
-		this.log(JSON.stringify((await patients.reduce(async (p: Promise<any>, pat: PatientDto) => {
-			const prev = await p
-			try {
-				return prev.concat([await api.patienticc.share(user, pat.id!, user.healthcarePartyId!, [hcpId], { [hcpId]: ['all'] })])
-			} catch (e) {
-				console.log(e)
-				return prev
-			}
-		}
-			, Promise.resolve([]))).map((x: any) => x.statuses), undefined, ' '))
-	})
-
-vorpal
 	.command('shareall [hcpIds...]', 'Share with hcp ids')
 	.action(async function(this: CommandInstance, args: Args) {
 		let user = await api.usericc.getCurrentUser()
 
+		const hcpIds = args.hcpIds as string[]
 		const allIds = await api.patienticc.listPatientsIds(user.healthcarePartyId, undefined, undefined, 50000)
 
 		chunk(allIds.rows, 100).reduce(async (p, ids) => {
 			await p
 			const patients = await api.patienticc.getPatientsWithUser(user, new ListOfIdsDto({ ids })) // Get them to fix them
 
+			this.log('Treating 100 patients')
+
 			this.log(JSON.stringify((await patients.reduce(async (p: Promise<any>, pat: PatientDto) => {
 				const prev = await p
 				try {
-					return prev.concat([await api.patienticc.export(user, pat.id!, user.healthcarePartyId!)])
+					return prev.concat([await api.patienticc.share(user, pat.id!, user.healthcarePartyId!, hcpIds, hcpIds.reduce((map, hcpId) => Object.assign(map, { [hcpId]: ['all'] }), {}))])
 				} catch (e) {
 					console.log(e)
 					return prev
 				}
-			}, Promise.resolve([])))))
+			}
+				, Promise.resolve([]))).map((x: any) => x.statuses), undefined, ' '))
 
 		}, Promise.resolve())
 	})
